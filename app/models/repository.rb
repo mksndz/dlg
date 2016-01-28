4a13059c5c1710e497f0ebb3065305cca7860f26
Mike Kanning <mikekanning@gmail.com>
2016-01-27 21:04:30 -0500

    indexing info in model for repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -5,4 +5,25 @@ class Repository < ActiveRecord::Base
   has_many :dpla_collections, -> { where dpla: true }, class_name: 'Collection'
   has_many :items, through: :collections
 
+  validates_presence_of :slug, :title
+
+  searchable do
+
+    string :slug
+    string :homepage_url
+    string :directions_url
+
+    boolean :in_georgia
+    boolean :public
+
+    text :title
+    text :description
+    text :teaser
+    text :short_description
+    text :address
+    text :strengths
+    text :contact
+
+  end
+
 end
