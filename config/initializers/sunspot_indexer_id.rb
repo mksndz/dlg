b9024af9c1d3e94a5c790a25251a9ea6a39dbba2
Mike Kanning <mikekanning@gmail.com>
2016-01-29 16:37:52 -0500

    slight refactor to sunspot patch to enable retaining proper id values after updates

--- config/initializers/sunspot_indexer_id.rb
+++ config/initializers/sunspot_indexer_id.rb
@@ -1,13 +1,11 @@
 # for using a different value for the id field of your Solr documents
+# only applies when the model has a +slug+ method available and stored in Solr
 Sunspot::Indexer.module_eval do
-
-  # define models
-  SLUGGED_MODELS = [Repository, Collection, Item]
 
   alias :old_prepare_full_update :prepare_full_update
   def prepare_full_update(model)
     document = old_prepare_full_update(model)
-    if SLUGGED_MODELS.include? model.class
+    if model.respond_to? :slug
       document.field_by_name(:sunspot_id_s).value = document.field_by_name(:id).value
       document.field_by_name(:id).value = document.field_by_name(:slug_s).value
     end
@@ -20,7 +18,7 @@ Sunspot::Indexer.module_eval do
   def remove(*models)
     @connection.delete_by_id(
         models.map do |model|
-          SLUGGED_MODELS.include? model.class ? model.slug : model.id
+          model.respond_to? :slug ? model.slug : model.id
         end
     )
   end
@@ -32,7 +30,7 @@ end
 # the default Sunspot functionality will work
 class Sunspot::Search::Hit
   def initialize(raw_hit, highlights, search)
-    raw_hit['id'] = raw_hit['sunspot_id_s'] if SLUGGED_MODELS.include? raw_hit['class_name'].first
+    raw_hit['id'] = raw_hit['sunspot_id_s'] if raw_hit['slug_s']
     @class_name, @primary_key = *raw_hit['id'].match(/([^ ]+) (.+)/)[1..2]
     @score = raw_hit['score']
     @search = search
