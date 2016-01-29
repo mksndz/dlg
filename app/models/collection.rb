a2ecb35cdbbf1804aa15d55539992f27e2d4f82f
Mike Kanning <mikekanning@gmail.com>
2016-01-29 16:06:48 -0500

    indexing routines for Collections and Items

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -5,4 +5,44 @@ class Collection < ActiveRecord::Base
   has_many :dpla_items, -> { where dpla: true }, class_name: 'Item'
   belongs_to :repository
 
+  validates_presence_of :slug, :display_title
+
+  searchable do
+
+    string :slug
+
+    # set empty proxy id field so sunspot knows about it
+    # value is set prior to save
+    # see monkeypatch @ config/initializers/sunspot_indexer_id.rb
+    string :sunspot_id do
+      ''
+    end
+
+    string :in_repository do
+      self.repository.title
+    end
+
+    boolean :in_georgia
+    boolean :public
+
+    text :display_title
+    text :teaser
+    text :short_description
+
+    text :dc_title
+    text :dc_format
+    text :dc_publisher
+    text :dc_identifier
+    text :dc_rights
+    text :dc_contributor
+    text :dc_coverage_t
+    text :dc_coverage_s
+    text :dc_date
+    text :dc_source
+    text :dc_subject
+    text :dc_type
+    text :dc_description
+
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,38 @@ class Item < ActiveRecord::Base
   belongs_to :collection, counter_cache: true
   has_one :repository, through: :collection
 
+  searchable do
+
+    string :slug
+
+    # set empty proxy id field so sunspot knows about it
+    # value is set prior to save
+    # see monkeypatch @ config/initializers/sunspot_indexer_id.rb
+    string :sunspot_id do
+      ''
+    end
+
+    boolean :dpla
+    boolean :public
+
+    string :in_collection do
+      self.collection.display_title
+    end
+
+    text :dc_title
+    text :dc_format
+    text :dc_publisher
+    text :dc_identifier
+    text :dc_rights
+    text :dc_contributor
+    text :dc_coverage_t
+    text :dc_coverage_s
+    text :dc_date
+    text :dc_source
+    text :dc_subject
+    text :dc_type
+    text :dc_description
+
+  end
+
 end
