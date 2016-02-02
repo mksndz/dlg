add155ef89ad187e63d28a559b369949204cadb3
Mike Kanning <mikekanning@gmail.com>
2016-02-02 11:17:43 -0500

    Controllers for Item and Collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,30 +6,90 @@ class CollectionsController < ApplicationController
 
   end
 
+  def show
+
+    find_collection
+
+  end
+
   def new
+
+    @collection = Collection.new
 
   end
 
   def create
 
+    @collection = Collection.new collection_params
+
+    if @collection.save
+      redirect_to @collection, notice: 'Collection created'
+    else
+      redirect_to new_collection_path, alert: 'Error creating collection'
+    end
+
   end
 
   def edit
+
+    find_collection
 
   end
 
   def update
 
+    if find_collection.update(collection_params)
+      redirect_to @collection, notice: 'Collection updated'
+    else
+      redirect_to :new, alert: 'Error creating collection'
+    end
+
   end
 
   def destroy
+
+    if find_collection.destroy
+      redirect_to collections_path, notice: 'Collection destroyed.'
+    else
+      redirect_to collections_path, alert: 'Collection could not be destroyed.'
+    end
 
   end
 
   private
 
+  def find_collection
+    @collection = Collection.find(params[:id])
+  end
+
   def collection_params
-    params.require(:collection).permit!
+    params.require(:collection).permit(
+        :repository_id,
+        :slug,
+        :dpla,
+        :public,
+        :in_georgia,
+        :remote,
+        :display_title,
+        :short_description,
+        :teaser,
+        :color,
+        :other_repositories,
+        :date_range,
+        :dc_format      => [],
+        :dc_publisher   => [],
+        :dc_identifier  => [],
+        :dc_rights      => [],
+        :dc_contributor => [],
+        :dc_coverage_s  => [],
+        :dc_coverage_t  => [],
+        :dc_date        => [],
+        :dc_source      => [],
+        :dc_subject     => [],
+        :dc_type        => [],
+        :dc_description => []
+    )
   end
 
 end
+
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,30 +6,85 @@ class ItemsController < ApplicationController
 
   end
 
+  def show
+
+    find_item
+
+  end
+
   def new
+
+    @item = Item.new
 
   end
 
   def create
 
+    @item = Item.new item_params
+
+    if @item.save
+      redirect_to @item, notice: 'Item created'
+    else
+      redirect_to new_item_path, alert: 'Error creating item'
+    end
+
   end
 
   def edit
+
+    find_item
 
   end
 
   def update
 
+    if find_item.update(item_params)
+      redirect_to @item, notice: 'Item updated'
+    else
+      redirect_to :new, alert: 'Error creating item'
+    end
+
   end
 
   def destroy
+
+    if find_item.destroy
+      redirect_to items_path, notice: 'Item destroyed.'
+    else
+      redirect_to items_path, alert: 'Item could not be destroyed.'
+    end
 
   end
 
   private
 
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
-    params.require(:item).permit!
+    params.require(:item).permit(
+        :collection_id,
+        :slug,
+        :dpla,
+        :public,
+        :in_georgia,
+        :other_collections,
+        :date_range,
+        :dc_format      => [],
+        :dc_publisher   => [],
+        :dc_identifier  => [],
+        :dc_rights      => [],
+        :dc_contributor => [],
+        :dc_coverage_s  => [],
+        :dc_coverage_t  => [],
+        :dc_date        => [],
+        :dc_source      => [],
+        :dc_subject     => [],
+        :dc_type        => [],
+        :dc_description => []
+    )
   end
 
 end
+
