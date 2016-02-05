52992e5bf602fbc141e7952ef5bf297376cc40b7
Mike Kanning <mikekanning@gmail.com>
2016-02-05 17:39:29 -0500

    temp fix for slug checking regex

--- app/models/concerns/slugged.rb
+++ app/models/concerns/slugged.rb
@@ -1,10 +1,16 @@
 module Slugged
   extend ActiveSupport::Concern
 
+  # typical slugs
+  # gcfa_gtaa_gtaa88-32-0000016
+  # geh_maddox_26
+  # zhw_hancockarchives_deeds1
+
   included do
     validates_presence_of :slug
     validates_uniqueness_of :slug
-    # todo fix validates_format_of :slug, without: /[^\w\/]|[!\(\)\.]+/, message: 'is not URL friendly'
+    # no spaces todo devise a better regex for use here
+    validates_format_of :slug, without: /\s/, message: 'is not URL friendly'
   end
 
 end
