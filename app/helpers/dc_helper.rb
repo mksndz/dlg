8ecdbca72f7ff77dbefaa4e8903348d3ca9dbb81
Mike Kanning <mikekanning@gmail.com>
2016-02-10 10:07:11 -0500

    create a DcHelper to store dc field names in one place

---
+++ app/helpers/dc_helper.rb
@@ -0,0 +1,24 @@
+module DcHelper
+
+  def dc_fields
+    %w(
+      dc_title
+      dc_format
+      dc_publisher
+      dc_identifier
+      dc_rights
+      dc_contributor
+      dc_coverage_t
+      dc_coverage_s
+      dc_date
+      dc_source
+      dc_subject
+      dc_type
+      dc_description
+      dc_creator
+      dc_language
+      dc_relation
+    )
+  end
+
+end
