12a7513449f88b8397b6682743fa36d744d7769f
Mike Kanning <mikekanning@gmail.com>
2016-02-16 15:54:42 -0500

    fix batch relation to item to do cascading deletes

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,7 +1,7 @@
 class Batch < ActiveRecord::Base
 
   belongs_to :user
-  has_many :batch_items, table_name: 'items'
+  has_many :batch_items, table_name: 'items', dependent: :destroy
 
   searchable do
 
