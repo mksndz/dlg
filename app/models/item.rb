dcdf306cf9d23c36378bfa8283303852c9701124
Mike Kanning <mikekanning@gmail.com>
2016-02-16 20:47:28 -0500

    nice xml rendering for Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,23 @@
 class Item < AbstractItem
   include ItemIndexing
+
+  def to_xml(options = {})
+    default_options = {
+        dasherize: false,
+        # fields to not include
+        except: [
+          :batch_id,
+          :collection_id,
+          :created_at,
+          :updated_at,
+          :other_collections
+        ],
+        include: [
+            collection: { only: [ :slug ] },
+            repository: { only: [ :slug ] }
+        ]
+    }
+    super(options.merge!(default_options))
+  end
+
 end
