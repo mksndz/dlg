7d0b1a74147d33bab09bc4bf1dd38f691dcdf7cf
Mike Kanning <mikekanning@gmail.com>
2016-02-26 14:53:54 -0500

    remove counter cache from Repo

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,7 @@
 class Repository < ActiveRecord::Base
   include Slugged
 
-  has_many :collections, dependent: :destroy, counter_cache: true
+  has_many :collections, dependent: :destroy
   has_many :items, through: :collections
 
   validates_presence_of :title
