77d4743c1bef9409b2b1818af5e30cd76a581de3
Mike Kanning <mikekanning@gmail.com>
2016-03-23 10:04:28 -0400

    add additional User class for Admins

---
+++ app/models/admin.rb
@@ -0,0 +1,7 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :lockable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
+
--- app/models/admin/admin.rb
+++
@@ -1,9 +0,0 @@
-module Admin
-  class Admin < ActiveRecord::Base
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :invitable, :database_authenticatable, :lockable,
-           :recoverable, :rememberable, :trackable, :validatable
-  end
-end
-
