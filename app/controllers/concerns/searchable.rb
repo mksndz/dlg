c1b1c9ca82c04c721aad5bfd16fc0dada662db99
Mike Kanning <mikekanning@gmail.com>
2016-03-29 14:29:27 -0400

    move to data hash for results view

--- app/controllers/concerns/searchable.rb
+++ app/controllers/concerns/searchable.rb
@@ -9,14 +9,15 @@ module Searchable
   end
 
   def results
+    @data = {}
     if current_admin.super?
       s = search_class.search(params)
-      @results = s.results
-      # @count = s.total
+      @data[:results] = s.results
+      @data[:count] = s.total
     else
       if params[:search]
         # todo user limits
-        @results = search_class.search params
+        @data[:results] = search_class.search params
       else
         # collection_ids = current_admin.collection_ids || []
         # collection_ids += current_admin.repositories.map { |r| r.collection_ids }
@@ -26,10 +27,7 @@ module Searchable
         #              .order(sort_column + ' ' + sort_direction)
         #              .page(params[:page])
       end
-
     end
-
-    render :results
   end
 
   def search_class
