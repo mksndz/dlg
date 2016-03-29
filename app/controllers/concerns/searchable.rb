b1005ac926d64d779ab3de563291ccf6491ff3ca
Mike Kanning <mikekanning@gmail.com>
2016-03-29 16:23:18 -0400

    basic admin user search restrictions

--- app/controllers/concerns/searchable.rb
+++ app/controllers/concerns/searchable.rb
@@ -15,24 +15,45 @@ module Searchable
       @data[:results] = s.results
       @data[:count] = s.total
     else
-      if params[:search]
-        # todo user limits
-        @data[:results] = search_class.search params
-      else
-        # collection_ids = current_admin.collection_ids || []
-        # collection_ids += current_admin.repositories.map { |r| r.collection_ids }
-        # @items = Item
-        #              .includes(:collection)
-        #              .where(collection: collection_ids.flatten)
-        #              .order(sort_column + ' ' + sort_direction)
-        #              .page(params[:page])
-      end
+      # todo user limits
+      @data[:results] = search_class.search basic_user_params
     end
   end
 
   def search_class
     search_class_name = controller_path.singularize + '_search'
     search_class_name.classify.constantize
+  end
+
+  def basic_user_params
+    new_params = params
+    if params[:collection_id] and params[:collection_id].empty?
+      new_params[:collection_id] = current_admin.collection_ids
+    else
+      unless check_basic_admin_params throw ArgumentError
+    end
+
+    if params[:repository_id] and params[:repository_id].empty?
+      new_params[:repository_id] = current_admin.repository_ids
+    else
+      unless check_basic_admin_params throw ArgumentError
+    end
+
+    new_params
+  end
+
+  def basic_admin_collections
+    collection_ids = current_admin.collection_ids || []
+    collection_ids += current_admin.repositories.map { |r| r.collection_ids }
+    collection_ids
+  end
+
+  def check_basic_admin_collection_param
+    basic_admin_collections.includes? params[:collection_id]
+  end
+
+  def check_basic_admin_repository_param
+    current_admin.repository_idss.includes? params[:repository_id]
   end
 
 end
