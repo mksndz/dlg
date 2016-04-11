36449f46b3273ce5e317159034bc399f5f7b93d2
Mike Kanning <mikekanning@gmail.com>
2016-04-11 12:24:31 -0400

    restyle resend unlock email form

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -316,6 +316,6 @@ Rails.application.config.to_prepare do
   Devise::SessionsController.layout 'devise'
   # Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? "application" : "devise" }
   # Devise::ConfirmationsController.layout "devise"
-  # Devise::UnlocksController.layout "devise"
+  Devise::UnlocksController.layout 'devise'
   Devise::PasswordsController.layout 'devise'
 end
