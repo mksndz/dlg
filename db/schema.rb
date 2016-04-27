770dd6ef4683d350a5598f6f1787eba42a36a586
Mike Kanning <mikekanning@gmail.com>
2016-04-27 08:08:54 -0400

    enable postgres hstore extension

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413125340) do
+ActiveRecord::Schema.define(version: 20160427120251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "batch_items", force: :cascade do |t|
     t.boolean  "dpla",                  default: false, null: false
