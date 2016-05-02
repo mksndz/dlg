09a8b7479fa1b2d38589dff5b78f6e3686765d84
Mike Kanning <mikekanning@gmail.com>
2016-05-02 14:05:18 -0400

    reduce db queries executed when checking abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,52 +3,60 @@ class Ability
 
   def initialize(user)
 
+    roles = user.roles.pluck(:name)
+
     can :manage, :catalog
     can :manage, :bookmarks
 
-    if user.super?
+    if roles.include? 'super'
+
       can :manage, :all
-    end
 
-    if user.basic?
+    else
 
-      can [:index, :show, :new, :create, :edit, :update], Repository do |repository|
-        user.repositories.include?(repository)
+      if roles.include? 'basic'
+
+        can [:index, :show, :new, :create, :edit, :update], Repository do |repository|
+          user.repositories.include?(repository)
+        end
+
+        can [:index, :show, :new, :create, :edit, :update], Collection do |collection|
+          user.repositories.include?(collection.repository) ||
+              user.collections.include?(collection)
+        end
+
+        can [:index, :show, :new, :create, :edit, :update, :copy, :destroy, :search, :results], Item do |item|
+          user.repositories.include?(item.repository) ||
+              user.collections.include?(item.collection)
+        end
+
+        can [:show, :edit, :update, :destroy, :recreate], Batch, user_id: user.id
+        can [:index, :new, :create], Batch
+        can [:index, :show, :new, :create], BatchItem
+        can [:edit, :update, :destroy], BatchItem, { batch: { user_id: user.id }  }
+
       end
 
-      can [:index, :show, :new, :create, :edit, :update], Collection do |collection|
-        user.repositories.include?(collection.repository) ||
-            user.collections.include?(collection)
+      if roles.include? 'coordinator'
+
+        # User with Coordinator Role can create new Users
+        can [:new, :create], User
+
+        # can also manage Users they created
+        can [:index, :show, :edit, :update, :destroy], User, creator_id: user.id
+
       end
 
-      can [:index, :show, :new, :create, :edit, :update, :copy, :destroy, :search, :results], Item do |item|
-        user.repositories.include?(item.repository) ||
-            user.collections.include?(item.collection)
+      if roles.include? 'committer'
+
+        # User with Committer Role can commit their own Batches
+        can :commit, Batch, user_id: user.id
+
       end
 
-      can [:show, :edit, :update, :destroy, :recreate], Batch, user_id: user.id
-      can [:index, :new, :create], Batch
-      can [:index, :show, :new, :create], BatchItem
-      can [:edit, :update, :destroy], BatchItem, { batch: { user_id: user.id }  }
-
     end
 
-    if user.coordinator?
 
-      # User with Coordinator Role can create new Users
-      can [:new, :create], User
-
-      # can also manage Users they created
-      can [:index, :show, :edit, :update, :destroy], User, creator_id: user.id
-
-    end
-
-    if user.committer?
-
-      # User with Committer Role can commit their own Batches
-      can :commit, Batch, user_id: user.id
-
-    end
 
   end
 
