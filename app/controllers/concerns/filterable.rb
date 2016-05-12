d765ab32be19261af70f32b71142681284844844
Mike Kanning <mikekanning@gmail.com>
2016-05-12 13:13:29 -0400

    optimize database loading for Filter concern

--- app/controllers/concerns/filterable.rb
+++ app/controllers/concerns/filterable.rb
@@ -4,17 +4,16 @@ module Filterable
   private
 
   def set_filter_options(parameters = [])
-    # todo refactor to use case here? rethink
     @search_options = {}
     @search_options[:public] = [['Public or Not Public', ''],['Public', '1'],['Not Public', '0']] if parameters.include? :public
     @search_options[:status] = [%w(Committed committed), %w(Pending pending)] if parameters.include? :status
     if current_user.super?
-      @search_options[:collections] = Collection.all.order(:display_title) if parameters.include? :collection
-      @search_options[:repositories] = Repository.all.order(:title) if parameters.include? :repository
+      @search_options[:collections] = Collection.select(:id, :display_title).order(:display_title) if parameters.include? :collection
+      @search_options[:repositories] = Repository.select(:id, :title).order(:title) if parameters.include? :repository
       @search_options[:users] = User.all if parameters.include? :user
-    elsif current_user.basic?
-      @search_options[:collections] = Collection.where(id: current_user.collection_ids).order(:display_title) if parameters.include? :collection
-      @search_options[:repositories] = Repository.where(id: current_user.repository_ids).order(:title) if parameters. include? :repository
+    else
+      @search_options[:collections] = Collection.select(:id, :display_title).where(id: current_user.collection_ids).order(:display_title) if parameters.include? :collection
+      @search_options[:repositories] = Repository.select(:id, :title).where(id: current_user.repository_ids).order(:title) if parameters. include? :repository
       @search_options[:users] = User.where(creator_id: current_user.id) if parameters.include? :user
     end
   end
