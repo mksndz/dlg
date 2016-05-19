1042ad213aa217a1d06f44c062646047f530c729
Mike Kanning <mikekanning@gmail.com>
2016-05-19 13:56:40 -0400

    remove simple search bar to avoid sorting param issues

--- spec/features/items_feature_spec.rb
+++ spec/features/items_feature_spec.rb
@@ -6,21 +6,4 @@ feature 'Item Management' do
 
   let(:super_user) { Fabricate :super }
 
-  scenario 'when user does a search from a sorted index list, the search should not fail' do
-
-    login_as super_user, scope: :user
-
-    Fabricate :item
-
-    visit items_path
-
-    click_link I18n.t('meta.defaults.labels.columns.slug')
-
-    fill_in 'search for', with: 'anything'
-    click_button 'Search'
-
-    expect(page).to have_text 'Limit your search'
-
-  end
-
 end
