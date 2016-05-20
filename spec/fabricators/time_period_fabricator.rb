7e3ab6e3152e545e731bac503862ec6498686346
Mike Kanning <mikekanning@gmail.com>
2016-05-20 09:47:13 -0400

    time_period unit specs

--- spec/fabricators/time_period_fabricator.rb
+++ spec/fabricators/time_period_fabricator.rb
@@ -2,4 +2,6 @@ require 'faker'
 
 Fabricator(:time_period) do
   name { Faker::Hipster.word }
+  start { Faker::Number.number(4) }
+  finish { Faker::Number.number(4) }
 end
--- spec/models/time_period_spec.rb
+++ spec/models/time_period_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe TimePeriod, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has none to begin with' do
+    expect(TimePeriod.count).to eq 0
+  end
+
+  it 'has one after adding one' do
+    Fabricate(:time_period)
+    expect(TimePeriod.count).to eq 1
+  end
+
+  it 'has a name' do
+    s = Fabricate(:time_period)
+    expect(s.name).not_to be_empty
+    expect(s.name).to be_a String
+  end
+
+  it 'has a start' do
+    s = Fabricate(:time_period)
+    expect(s.start).not_to be_nil
+    expect(s.start).to be_a Integer
+  end
+
+  it 'has a finish' do
+    s = Fabricate(:time_period)
+    expect(s.finish).not_to be_nil
+    expect(s.finish).to be_a Integer
+  end
+
+  it 'can have Collections' do
+    s = Fabricate(:time_period) {
+      collections(count: 1)
+    }
+    expect(s).to respond_to 'collections'
+    expect(s.collections.first).to be_a Collection
+  end
+
 end
