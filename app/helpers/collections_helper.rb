b884b1856aae80c28eabe33a6e552e66def4290b
Mike Kanning <mikekanning@gmail.com>
2016-05-25 18:48:46 -0400

    add but do not use helper to show collection item counts

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -2,4 +2,13 @@ module CollectionsHelper
   def invalid_items_count(collection)
     collection.items.select { |i| !i.valid? }.count
   end
+
+  def solr_items_count(collection)
+    Item.search do
+      with(:collection_id, collection.id)
+      adjust_solr_params do |params|
+        params[:q] = ''
+      end
+    end.total
+  end
 end
