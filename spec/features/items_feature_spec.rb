258b1944f453b7f0c61ba1995b00cfb64ca621f3
Mike Kanning <mikekanning@gmail.com>
2016-05-27 15:43:17 -0400

    feature spec for item removing other_collection value

--- spec/features/items_feature_spec.rb
+++ spec/features/items_feature_spec.rb
@@ -56,4 +56,34 @@ feature 'Item Management' do
 
   end
 
+  scenario 'super user saves a new item removing other_collection value' do
+
+    login_as super_user, scope: :user
+
+    c0 = Fabricate(:collection)
+    c1 = Fabricate(:collection) {
+      items(count:1)
+    }
+
+    c1.items.first.other_collections = [c0.id]
+
+    visit items_path
+
+    click_on I18n.t('meta.defaults.actions.edit')
+
+    fill_in I18n.t('activerecord.attributes.item.slug'),                  with: 'test'
+    fill_in I18n.t('activerecord.attributes.item.dcterms_temporal'),      with: '2000'
+    fill_in I18n.t('activerecord.attributes.item.dcterms_spatial'),       with: 'Georgia'
+    fill_in I18n.t('activerecord.attributes.item.dcterms_contributor'),   with: 'DLG'
+    fill_in I18n.t('activerecord.attributes.item.dcterms_type'),          with: 'Text'
+    fill_in I18n.t('activerecord.attributes.item.dc_right'),              with: 'None'
+
+    select '', from: I18n.t('activerecord.attributes.item.other_collections')
+
+    click_button I18n.t('meta.defaults.actions.save')
+
+    expect(page).to have_current_path item_path(c1.items.first)
+
+  end
+
 end
