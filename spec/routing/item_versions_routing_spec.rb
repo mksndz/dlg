251bc759bd0492b812f0752f3250dcd16ee0ed78
Mike Kanning <mikekanning@gmail.com>
2016-06-01 15:02:08 -0400

    rename routing spec file

---
+++ spec/routing/item_versions_routing_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ItemVersionsController, type: :routing do
+  describe 'item versions' do
+
+    it 'routes to #diff' do
+      expect(get: '/items/1/versions/1/diff').to route_to(controller: 'item_versions', action: 'diff', item_id: '1', id: '1')
+    end
+
+    it 'routes to #rollback' do
+      expect(patch: '/items/1/versions/1/rollback').to route_to(controller: 'item_versions', action: 'rollback', item_id: '1', id: '1')
+    end
+
+  end
+end
