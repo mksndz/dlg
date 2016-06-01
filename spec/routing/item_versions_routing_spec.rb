fc0f8bef906e2700a05e6db758c7698c373fb3e1
Mike Kanning <mikekanning@gmail.com>
2016-06-01 15:02:47 -0400

    update route names in spec

--- spec/routing/item_versions_routing_spec.rb
+++ spec/routing/item_versions_routing_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe ItemVersionsController, type: :routing do
   describe 'item versions' do
 
     it 'routes to #diff' do
-      expect(get: '/items/1/versions/1/diff').to route_to(controller: 'item_versions', action: 'diff', item_id: '1', id: '1')
+      expect(get: '/items/1/item_versions/1/diff').to route_to(controller: 'item_versions', action: 'diff', item_id: '1', id: '1')
     end
 
     it 'routes to #rollback' do
-      expect(patch: '/items/1/versions/1/rollback').to route_to(controller: 'item_versions', action: 'rollback', item_id: '1', id: '1')
+      expect(patch: '/items/1/item_versions/1/rollback').to route_to(controller: 'item_versions', action: 'rollback', item_id: '1', id: '1')
     end
 
   end
