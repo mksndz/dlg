f5017f987c79ac934ffb2b90fbaf761af8cf8de8
Mike Kanning <mikekanning@gmail.com>
2016-06-13 17:15:37 -0400

    update paper_trail gem and address changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   include Blacklight::Controller
   layout 'blacklight'
 
+  before_filter :set_paper_trail_whodunnit
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user_for_paper_trail
+    current_user
+  end
 
 end
