f5cd17134c5f98d69e80e4afb26af43e1a34396e
Mike Kanning <mikekanning@gmail.com>
2016-06-15 11:18:14 -0400

    add fabricators for committed batches

--- spec/fabricators/batch_fabricator.rb
+++ spec/fabricators/batch_fabricator.rb
@@ -1,7 +1,32 @@
 require 'faker'
 
+test_results_hash_with_error = {'items':[{'batch_item':1,'item':1,'slug':'test_slug'}],'errors':[{'batch_item':2,'errors':	{'test_key':['test error message']}}]}
+test_results_hash = {'items':[{'batch_item':1,'item':1,'slug':'test_slug'}],'errors':[]}
+
 Fabricator(:batch) do
   name { Faker::Lorem.sentence(4) }
   notes { Faker::Lorem.sentence(10) }
   user { Fabricate(:user) }
+end
+
+Fabricator(:committed_batch_with_error, from: :batch) do
+  name { Faker::Lorem.sentence(4) }
+  notes { Faker::Lorem.sentence(10) }
+  user { Fabricate(:user) }
+  committed_at { Time.now }
+  queued_for_commit_at { Time.now }
+  batch_items(count: 2)
+  batch_items_count { 2 }
+  commit_results { test_results_hash_with_error }
+end
+
+Fabricator(:committed_batch, from: :batch) do
+  name { Faker::Lorem.sentence(4) }
+  notes { Faker::Lorem.sentence(10) }
+  user { Fabricate(:user) }
+  committed_at { Time.now }
+  queued_for_commit_at { Time.now }
+  batch_items(count: 1)
+  batch_items_count { 1 }
+  commit_results { test_results_hash }
 end
