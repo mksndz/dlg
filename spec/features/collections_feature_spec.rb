1b4173d8025561b6e2967e68be6b53e46ac8bf14
Mike Kanning <mikekanning@gmail.com>
2016-06-15 15:53:54 -0400

    collections feature specs

--- spec/features/collections_feature_spec.rb
+++ spec/features/collections_feature_spec.rb
@@ -4,6 +4,115 @@ Warden.test_mode!
 
 feature 'Collections Management' do
 
+  let(:super_user) { Fabricate :super }
+  let(:basic_user) { Fabricate :basic }
 
+  context 'for super user' do
+
+    before :each do
+      login_as super_user, scope: :user
+    end
+
+    scenario 'index shows all collections with actions' do
+
+      collection = Fabricate :collection
+      # another_collection = Fabricate :collection
+
+      visit collections_path
+
+      expect(page).to have_text collection.title
+      expect(page).to have_text collection.slug
+      expect(page).to have_text collection.repository.title
+      expect(page).to have_link I18n.t('meta.defaults.actions.view')
+      expect(page).to have_link I18n.t('meta.defaults.actions.edit')
+      expect(page).to have_link I18n.t('meta.defaults.actions.destroy')
+
+    end
+
+    scenario 'index page allows filtering by repository' do
+
+      repository = Fabricate :repository do
+        collections(count: 1)
+      end
+
+      other_repository = Fabricate :repository do
+        collections(count: 1)
+      end
+
+      visit collections_path
+
+      select repository.title, from: 'repository_id'
+      click_on I18n.t('meta.defaults.actions.filter')
+
+      expect(page).to have_text repository.title
+      within 'tbody' do
+        expect(page).not_to have_text other_repository.title
+      end
+
+    end
+
+    scenario 'index page allows filtering by public status' do
+
+      repository = Fabricate :repository do
+        collections(count: 2)
+      end
+
+      public_collection = repository.collections.first
+      not_public_collection = repository.collections.last
+
+      public_collection.public = true
+      public_collection.save
+
+      not_public_collection.public = false
+      not_public_collection.save
+
+      visit collections_path
+
+      select 'Public', from: 'public'
+      click_on I18n.t('meta.defaults.actions.filter')
+
+      expect(page).to have_text public_collection.slug
+      within 'tbody' do
+        expect(page).not_to have_text not_public_collection.slug
+      end
+
+    end
+
+    scenario 'index page allows sorting by item count' do
+
+      # todo
+
+    end
+
+  end
+
+  context 'for basic user' do
+
+    before :each do
+      login_as basic_user, scope: :user
+    end
+
+    scenario 'index page allows filtering by repository' do
+
+      repository = Fabricate :repository do
+        collections(count: 1)
+      end
+
+      other_repository = Fabricate :repository do
+        collections(count: 1)
+      end
+
+      basic_user.repositories << repository
+
+      visit collections_path
+
+      expect(page).to have_text repository.collections.first.title
+      within 'tbody' do
+        expect(page).not_to have_text other_repository.collections.first.title
+      end
+
+    end
+
+  end
 
 end
