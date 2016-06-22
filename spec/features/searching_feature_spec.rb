5eb36c892116d5fcab10540a94ae7a4b4b0521e9
Mike Kanning <mikekanning@gmail.com>
2016-06-22 16:12:03 -0400

    basic search feature test

--- spec/features/searching_feature_spec.rb
+++ spec/features/searching_feature_spec.rb
@@ -4,4 +4,41 @@ Warden.test_mode!
 
 feature 'Searching' do
 
+  let(:super_user) { Fabricate :super }
+
+  context 'for super user' do
+
+    before :each do
+      login_as super_user, scope: :user
+    end
+
+    scenario 'super user does a basic search and results are returned' do
+
+      Fabricate(:collection) { items(count:10 )}
+      Sunspot.commit
+
+      visit root_path
+
+      fill_in 'q', with: ''
+
+      within '.basic-search-well' do
+        click_on 'Search'
+      end
+
+      expect(page).to have_css('.document')
+
+    end
+
+  end
+
+  context 'for basic user' do
+
+    scenario '' do
+
+
+
+    end
+
+  end
+
 end
