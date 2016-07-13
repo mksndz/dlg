8432a7c7f3188239d7ad97e88b2db104ac1544f9
Mike Kanning <mikekanning@gmail.com>
2016-07-13 10:30:36 -0400

    set devise secret key to env var in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,13 +6,13 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'ac08455f94f68e5208a91ab9b7cf34a808dd16c7c27f65907edbc917af8df366e0c6d96d8a6620a070d4085c34721fda620863cee7c00796744f4ccb7e64000c'
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'uga.lag.test@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
