dfe8d7cec0f9a9cc8ba84fa635dc93599a80d8af
Mike Kanning <mikekanning@gmail.com>
2016-07-20 17:34:05 -0400

    remove user role rebuilding in migration

--- db/migrate/20160720163616_roles_remove_and_transform.rb
+++ db/migrate/20160720163616_roles_remove_and_transform.rb
@@ -1,21 +1,6 @@
 class RolesRemoveAndTransform < ActiveRecord::Migration
 
-
   def up
-
-    # copy roles to new style
-    User.all.each do |u|
-
-      roles = u.roles.pluck('name')
-
-      u.is_super = roles.include? 'super'
-      u.is_coordinator = roles.include? 'coordinator'
-      u.is_committer = roles.include? 'commiter'
-      u.is_uploader = roles.include? 'uploader'
-
-      u.save
-
-    end
 
     drop_join_table :roles, :users
     drop_table :roles
@@ -32,27 +17,7 @@ class RolesRemoveAndTransform < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    # define roles
-    Role.create!([
-                     { name: 'super' },
-                     { name: 'coordinator' },
-                     { name: 'committer' },
-                     { name: 'uploader' },
-                     { name: 'basic' }
-                 ])
-
-    # set roles for user
-    User.all.each do |u|
-
-      u.roles << Role.find_by_name('super') if u.super?
-      u.roles << Role.find_by_name('coordinator') if u.coordinator
-      u.roles << Role.find_by_name('committer') if u.committer
-      u.roles << Role.find_by_name('uploader') if u.uploader
-      u.roles << Role.find_by_name('basic')
-
-      u.save
-
-    end
+    create_join_table :roles, :users
 
   end
 
