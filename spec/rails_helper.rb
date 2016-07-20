b7e9f429cf892a47e756da18a22cf09e36b71ddd
Mike Kanning <mikekanning@gmail.com>
2016-07-20 15:16:17 -0400

    shush Devise depreciation warnings the right way

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,7 +58,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::ControllerHelpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   # clean up Solr index after test suite
   config.after(:suite){
