112753733b98e147ff21b2ff7c0e79d433ff72b6
Mike Kanning <mikekanning@gmail.com>
2016-08-03 17:07:11 -0400

    load turbolinks prior to blacklight JS to ensure proper binding of events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,16 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= require 'blacklight_advanced_search'
 //= require blacklight-maps
 //= require blacklight/blacklight
 //= require bootstrap/tooltip
 //= require bootstrap/popover
 //= require chosen-jquery
-//= require turbolinks
 //= require_tree .
 
 $(document).on('ready turbolinks:load', function() {
+
     $('select.advanced-search-facet-select').chosen();
     $('select.standardized-rights-select').chosen({
         allow_single_deselect: true
