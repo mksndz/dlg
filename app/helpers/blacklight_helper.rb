39e15f9a0dbd728fa6f66f3abd5fc60d7c84fb02
Mike Kanning <mikekanning@gmail.com>
2016-08-09 09:53:05 -0400

    page title improvements

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -5,4 +5,22 @@ module BlacklightHelper
     10
   end
 
+  def render_page_title
+    if content_for?(:page_title)
+      head_page_titleify content_for(:page_title)
+    elsif content_for?(:title)
+      head_page_titleify content_for(:title)
+    elsif @page_title
+      head_page_titleify @page_title
+    elsif application_name
+      application_name
+    end
+  end
+
+  private
+
+  def head_page_titleify(pre)
+    pre + ' - ' + application_name
+  end
+
 end
