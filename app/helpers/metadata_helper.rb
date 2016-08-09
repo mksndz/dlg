e033ff408ab7f518af4980c2ac15c290c04534e1
Mike Kanning <mikekanning@gmail.com>
2016-08-09 09:40:19 -0400

    add CC licenses to standardized rights statements

--- app/helpers/metadata_helper.rb
+++ app/helpers/metadata_helper.rb
@@ -29,60 +29,7 @@ module MetadataHelper
     )
   end
 
-  # def rights_statements
-  #   {
-  #       inc: [
-  #           'In Copyright',
-  #           'http://rightsstatements.org/vocab/InC/1.0/'
-  #       ],
-  #       inc_ow_eu: [
-  #           'In Copyright - EU Orphan Work',
-  #           'http://rightsstatements.org/vocab/InC-OW-EU/1.0/'
-  #       ],
-  #       inc_edu: [
-  #           'In Copyright - Educational Use Permitted',
-  #           'http://rightsstatements.org/vocab/InC-EDU/1.0/'
-  #       ],
-  #       inc_enc: [
-  #           'In Copyright - Non-Commercial Use Permitted',
-  #           'http://rightsstatements.org/vocab/InC-NC/1.0/'
-  #       ],
-  #       inc_ruu: [
-  #           'In Copyright - Rights-holder(s) Unlocatable or Unidentifiable',
-  #           'http://rightsstatements.org/vocab/InC-RUU/1.0/'
-  #       ],
-  #       noc_cr: [
-  #           'No Copyright - Contractual Restrictions',
-  #           'http://rightsstatements.org/vocab/NoC-CR/1.0/'
-  #       ],
-  #       noc_nc: [
-  #           'No Copyright - Non-Commercial Use Only',
-  #           'http://rightsstatements.org/vocab/NoC-NC/1.0/'
-  #       ],
-  #       noc_oklr: [
-  #           'No Copyright - Other Known Legal Restrictions',
-  #           'http://rightsstatements.org/vocab/NoC-OKLR/1.0/'
-  #       ],
-  #       noc_us: [
-  #           'No Copyright - United States',
-  #           'http://rightsstatements.org/vocab/NoC-US/1.0/'
-  #       ],
-  #       cne: [
-  #           'Copyright Not Evaluated',
-  #           'http://rightsstatements.org/vocab/CNE/1.0/'
-  #       ],
-  #       und: [
-  #           'Copyright Undetermined',
-  #           'http://rightsstatements.org/vocab/UND/1.0/'
-  #       ],
-  #       nkc: [
-  #           'No Known Copyright',
-  #           'http://rightsstatements.org/vocab/NKC/1.0/'
-  #       ],
-  #   }
-  # end
-
-  def rights_statements
+  def rights_statements_dot_org
     {
         'In Copyright'                                                    => 'http://rightsstatements.org/vocab/InC/1.0/',
         'In Copyright - EU Orphan Work'                                   => 'http://rightsstatements.org/vocab/InC-OW-EU/1.0/',
@@ -95,7 +42,20 @@ module MetadataHelper
         'No Copyright - United States'                                    => 'http://rightsstatements.org/vocab/NoC-US/1.0/',
         'Copyright Not Evaluated'                                         => 'http://rightsstatements.org/vocab/CNE/1.0/',
         'Copyright Undetermined'                                          => 'http://rightsstatements.org/vocab/UND/1.0/',
-        'No Known Copyright'                                              => 'http://rightsstatements.org/vocab/NKC/1.0/'
+        'No Known Copyright'                                              => 'http://rightsstatements.org/vocab/NKC/1.0/',
+    }
+  end
+
+  def creative_commons_licenses
+    {
+        'Creative Commons CC0 - Public Domain Dedication '                      => 'https://creativecommons.org/publicdomain/zero/1.0/',
+        'Creative Commons Public Domain Mark'                                   => 'https://creativecommons.org/publicdomain/mark/1.0/',
+        'Creative Commons CC0 - No Rights Reserved'                             => 'https://creativecommons.org/share-your-work/public-domain/cc0/',
+        'Creative Commons CC BY-NC-SA - Attribution-NonCommercial-ShareAlike'   => 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+        'Creative Commons CC BY-NC - Attribution-NonCommercial'                 => 'https://creativecommons.org/licenses/by-nc/4.0/',
+        'Creative Commons CC BY-ND - Attribution-NoDerivatives'                 => 'https://creativecommons.org/licenses/by-nd/4.0/',
+        'Creative Commons CC BY-SA - Attribution-ShareAlike'                    => 'https://creativecommons.org/licenses/by-sa/4.0/',
+        'Creative Commons CC BY - Attribution'                                  => 'https://creativecommons.org/licenses/by/4.0/',
     }
   end
 
@@ -105,9 +65,9 @@ module MetadataHelper
     end
   end
 
-  def rights_statements_for_select
-    rights_statements.map do |name, uri|
-      ["#{name} (#{uri}}", name]
+  def all_rights_statements_for_select
+    rights_statements_dot_org.merge(creative_commons_licenses).map do |name, uri|
+      ["#{name} (#{uri})", name]
     end
   end
 
