8493519148ed2ded5f9d63f974acbd223327c7ee
Mike Kanning <mikekanning@gmail.com>
2016-08-15 12:03:53 -0400

    clean up batch fabricator

--- spec/fabricators/batch_fabricator.rb
+++ spec/fabricators/batch_fabricator.rb
@@ -1,18 +1,15 @@
 require 'faker'
 
-test_results_hash_with_error = {'items':[{'batch_item':1,'item':1,'slug':'test_slug'}],'errors':[{'batch_item':2,'errors':	{'test_key':['test error message']}}]}
+test_results_hash_with_error = {'items':[{'batch_item':1,'item':1,'slug':'test_slug'}],'errors':[{'batch_item':2,'errors': {'test_key':['test error message']}}]}
 test_results_hash = {'items':[{'batch_item':1,'item':1,'slug':'test_slug'}],'errors':[]}
 
 Fabricator(:batch) do
-  name { Faker::Lorem.sentence(4) }
-  notes { Faker::Lorem.sentence(10) }
+  name { Faker::Lorem.sentence }
+  notes { Faker::Lorem.sentence }
   user { Fabricate(:user) }
 end
 
 Fabricator(:committed_batch_with_error, from: :batch) do
-  name { Faker::Lorem.sentence(4) }
-  notes { Faker::Lorem.sentence(10) }
-  user { Fabricate(:user) }
   committed_at { Time.now }
   queued_for_commit_at { Time.now }
   batch_items(count: 2)
@@ -21,9 +18,6 @@ Fabricator(:committed_batch_with_error, from: :batch) do
 end
 
 Fabricator(:committed_batch, from: :batch) do
-  name { Faker::Lorem.sentence(4) }
-  notes { Faker::Lorem.sentence(10) }
-  user { Fabricate(:user) }
   committed_at { Time.now }
   queued_for_commit_at { Time.now }
   batch_items(count: 1)
