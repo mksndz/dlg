d165d233aaaed343da8f1dde5b0d849eb1ea7b7e
Mike Kanning <mikekanning@gmail.com>
2017-01-24 15:25:17 -0500

    return explicit format specification in item controller test. figure out a way to permit that param value.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -275,7 +275,7 @@ RSpec.describe ItemsController, type: :controller do
     it 'return xml for the requested items' do
       item = Fabricate(:item)
       item2 = Fabricate(:item)
-      get :xml, { entities: "#{item.id},#{item2.id}" }
+      get :xml, { entities: "#{item.id},#{item2.id}", format: :xml }
       expect(response.content_type).to eq 'application/xml'
       expect(response.body).to include item.slug
     end
@@ -286,7 +286,7 @@ RSpec.describe ItemsController, type: :controller do
       item = Fabricate(:item)
       item2 = Fabricate(:item)
       expect {
-        delete :multiple_destroy, { entities: "#{item.id},#{item2.id}" }
+        delete :multiple_destroy, { entities: "#{item.id},#{item2.id}", format: :json }
       }.to change(Item, :count).by(-2)
     end
   end
