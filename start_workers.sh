c1b6b837c445b1bb1c4e6384190b8ac24547563d
Mike Kanning <mikekanning@gmail.com>
2017-01-25 14:41:29 -0500

    simple shell script to start workers. closes #94

---
+++ start_workers.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PIDFILE=./legacy_import_worker.pid BACKGROUND=yes QUEUE=items RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./xml_worker.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
