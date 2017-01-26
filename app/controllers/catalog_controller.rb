9e8a0327b81633431ef28dde0f73a9e9f1b5a03a
Mike Kanning <mikekanning@gmail.com>
2017-01-26 14:28:17 -0500

    show dlg_subject_personal in BL show view. closes #95

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -135,34 +135,35 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'slug_ss',                        label: I18n.t('meta.search.labels.item_id')
-    config.add_show_field 'record_id_ss',                   label: I18n.t('meta.search.labels.record_id')
-    config.add_show_field 'dcterms_title_display',          label: I18n.t('meta.search.labels.dcterms_title')
-    config.add_show_field 'collection_name_sms',            label: I18n.t('meta.search.labels.collection'), link_to_search: true
-    config.add_show_field 'repository_name_sms',            label: I18n.t('meta.search.labels.repository'), link_to_search: true
-    config.add_show_field 'dcterms_is_part_of_display',     label: I18n.t('meta.search.labels.dcterms_is_part_of')
-    config.add_show_field 'dcterms_description_display',    label: I18n.t('meta.search.labels.dcterms_description')
-    config.add_show_field 'dc_format_display',              label: I18n.t('meta.search.labels.dc_format')
-    config.add_show_field 'dcterms_identifier_display',     label: I18n.t('meta.search.labels.dcterms_identifier')
-    config.add_show_field 'dc_right_display',               label: I18n.t('meta.search.labels.dc_right'), helper_method: 'rights_icon_tag'
-    config.add_show_field 'dc_date_display',                label: I18n.t('meta.search.labels.dc_date')
-    config.add_show_field 'dc_relation_display',            label: I18n.t('meta.search.labels.dc_relation')
-    config.add_show_field 'dcterms_publisher_display',      label: I18n.t('meta.search.labels.dcterms_publisher')
-    config.add_show_field 'dcterms_contributor_display',    label: I18n.t('meta.search.labels.dcterms_contributor')
-    config.add_show_field 'dcterms_temporal_display',       label: I18n.t('meta.search.labels.dcterms_temporal')
-    config.add_show_field 'dcterms_spatial_display',        label: I18n.t('meta.search.labels.dcterms_spatial'), link_to_search: :location_facet
-    config.add_show_field 'dcterms_provenance_display',     label: I18n.t('meta.search.labels.dcterms_provenance')
-    config.add_show_field 'dcterms_subject_display',        label: I18n.t('meta.search.labels.dcterms_subject'), link_to_search: :subject_facet
-    config.add_show_field 'dcterms_type_display',           label: I18n.t('meta.search.labels.dcterms_type')
-    config.add_show_field 'dcterms_creator_display',        label: I18n.t('meta.search.labels.dcterms_creator'), link_to_search: :creator_facet
-    config.add_show_field 'dcterms_language_display',       label: I18n.t('meta.search.labels.dcterms_language')
-    config.add_show_field 'dcterms_is_shown_at_display',    label: I18n.t('meta.search.labels.dcterms_is_shown_at'), helper_method: 'linkify'
-    config.add_show_field 'dcterms_rights_holder_display',  label: I18n.t('meta.search.labels.dcterms_rights_holder')
-    config.add_show_field 'dcterms_bibliographic_citation', label: I18n.t('meta.search.labels.dcterms_bibliographic_citation')
-    config.add_show_field 'dcterms_extent_display',         label: I18n.t('meta.search.labels.dcterms_extent')
-    config.add_show_field 'dcterms_medium_display',         label: I18n.t('meta.search.labels.dcterms_medium')
-    config.add_show_field 'created_at_dts',                 label: I18n.t('meta.search.labels.created_at')
-    config.add_show_field 'updated_at_dts',                 label: I18n.t('meta.search.labels.updated_at')
+    config.add_show_field 'slug_ss',                                label: I18n.t('meta.search.labels.item_id')
+    config.add_show_field 'record_id_ss',                           label: I18n.t('meta.search.labels.record_id')
+    config.add_show_field 'dcterms_title_display',                  label: I18n.t('meta.search.labels.dcterms_title')
+    config.add_show_field 'collection_name_sms',                    label: I18n.t('meta.search.labels.collection'), link_to_search: true
+    config.add_show_field 'repository_name_sms',                    label: I18n.t('meta.search.labels.repository'), link_to_search: true
+    config.add_show_field 'dcterms_is_part_of_display',             label: I18n.t('meta.search.labels.dcterms_is_part_of')
+    config.add_show_field 'dcterms_description_display',            label: I18n.t('meta.search.labels.dcterms_description')
+    config.add_show_field 'dc_format_display',                      label: I18n.t('meta.search.labels.dc_format')
+    config.add_show_field 'dcterms_identifier_display',             label: I18n.t('meta.search.labels.dcterms_identifier')
+    config.add_show_field 'dc_right_display',                       label: I18n.t('meta.search.labels.dc_right'), helper_method: 'rights_icon_tag'
+    config.add_show_field 'dc_date_display',                        label: I18n.t('meta.search.labels.dc_date')
+    config.add_show_field 'dc_relation_display',                    label: I18n.t('meta.search.labels.dc_relation')
+    config.add_show_field 'dcterms_publisher_display',              label: I18n.t('meta.search.labels.dcterms_publisher')
+    config.add_show_field 'dcterms_contributor_display',            label: I18n.t('meta.search.labels.dcterms_contributor')
+    config.add_show_field 'dcterms_temporal_display',               label: I18n.t('meta.search.labels.dcterms_temporal')
+    config.add_show_field 'dcterms_spatial_display',                label: I18n.t('meta.search.labels.dcterms_spatial'), link_to_search: :location_facet
+    config.add_show_field 'dcterms_provenance_display',             label: I18n.t('meta.search.labels.dcterms_provenance')
+    config.add_show_field 'dcterms_subject_display',                label: I18n.t('meta.search.labels.dcterms_subject'), link_to_search: :subject_facet
+    config.add_show_field 'dcterms_type_display',                   label: I18n.t('meta.search.labels.dcterms_type')
+    config.add_show_field 'dcterms_creator_display',                label: I18n.t('meta.search.labels.dcterms_creator'), link_to_search: :creator_facet
+    config.add_show_field 'dcterms_language_display',               label: I18n.t('meta.search.labels.dcterms_language')
+    config.add_show_field 'dcterms_is_shown_at_display',            label: I18n.t('meta.search.labels.dcterms_is_shown_at'), helper_method: 'linkify'
+    config.add_show_field 'dcterms_rights_holder_display',          label: I18n.t('meta.search.labels.dcterms_rights_holder')
+    config.add_show_field 'dcterms_bibliographic_citation_display', label: I18n.t('meta.search.labels.dcterms_bibliographic_citation')
+    config.add_show_field 'dcterms_extent_display',                 label: I18n.t('meta.search.labels.dcterms_extent')
+    config.add_show_field 'dcterms_medium_display',                 label: I18n.t('meta.search.labels.dcterms_medium')
+    config.add_show_field 'dlg_subject_personal_display',           label: I18n.t('meta.search.labels.dlg_subject_personal')
+    config.add_show_field 'created_at_dts',                         label: I18n.t('meta.search.labels.created_at')
+    config.add_show_field 'updated_at_dts',                         label: I18n.t('meta.search.labels.updated_at')
 
 
     # ADVANCED SEARCH CONFIG
