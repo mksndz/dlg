9f8f94e6e925e5acf1a23b1824e26772185d2179
Mike Kanning <mikekanning@gmail.com>
2017-02-17 16:27:59 -0500

    break reindexing of class records into batches and reindex. closes #113

--- app/workers/reindexer.rb
+++ app/workers/reindexer.rb
@@ -3,16 +3,21 @@ class Reindexer
   @queue = :reindex
   @logger = Logger.new('./log/reindex.log')
 
+  REINDEX_BATCH_SIZE = 1000
+
   def self.perform(model)
 
     @logger.info "Reindexing #{model}"
     model_start_time = Time.now
     begin
-      model.constantize.reindex
+      model.constantize.find_in_batches(batch_size: REINDEX_BATCH_SIZE) do |batch|
+        Sunspot.index batch
+        Sunspot.commit
+      end
     rescue StandardError => e
       @logger.fatal "Reindexing #{model} failed: #{e}"
     end
-    Sunspot.commit
+
     model_finish_time = Time.now
     @logger.info "#{model} re-indexed in #{model_finish_time - model_start_time} seconds"
 
