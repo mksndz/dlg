b5022c12862e5f64ed85a4fc839a08a132d7ef1f
Mike Kanning <mikekanning@gmail.com>
2017-04-14 16:08:40 -0400

    some scripts to aid in solr deployment. optional solrcloud settings.

---
+++ update_metasolr_config.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# clear index
+curl http://metasolr.galib.uga.edu:8983/solr/blacklight-core/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+curl http://metasolr.galib.uga.edu:8983/solr/blacklight-core/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+
+# backup old configs
+ssh solr@metasolr cp /var/solr/data/blacklight-core/conf/schema.xml /var/solr/data/blacklight-core/conf/schema.old
+ssh solr@metasolr cp /var/solr/data/blacklight-core/conf/solrconfig.xml /var/solr/data/blacklight-core/conf/solrconfig.old
+
+# copy new config
+scp solr/schema.xml solr@sc0.galib.uga.edu:solr-6.4.1/server/solr/configsets/meta/
+scp solr/solrconfig.xml solr@sc0.galib.uga.edu:solr-6.4.1/server/solr/configsets/meta/
+
+# restart solr
+ssh solr@metasolr /opt/solr/bin/solr restart
