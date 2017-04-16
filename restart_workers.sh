7577af6509792bd370a30c86e7750d4de001f7b8
Mike Kanning <mikekanning@gmail.com>
2017-04-15 22:16:30 -0400

    script to kill workers and restart

--- restart_workers.sh
+++ restart_workers.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+kill -9 `cat legacy_import_worker.pid`
+kill -9 `cat commit_worker.pid`
+kill -9 `cat xml_worker.pid`
+kill -9 `cat reindex_worker.pid`
+
 PIDFILE=./legacy_import_worker.pid BACKGROUND=yes QUEUE=items RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./xml_worker.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
