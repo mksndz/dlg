5488b8990ba667a3a65db5982b411c6a508ffae7
Mike Kanning <mikekanning@gmail.com>
2017-04-15 22:40:00 -0400

    fix paths of solr schema in utility script

--- update_metasolr_config.sh
+++ update_metasolr_config.sh
@@ -5,8 +5,8 @@ curl http://metasolr.galib.uga.edu:8983/solr/blacklight-core/update --data '<del
 curl http://metasolr.galib.uga.edu:8983/solr/blacklight-core/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
 
 # backup old configs
-ssh solr@metasolr cp /var/solr/data/blacklight-core/conf/schema.xml /var/solr/data/blacklight-core/conf/schema.old
-ssh solr@metasolr cp /var/solr/data/blacklight-core/conf/solrconfig.xml /var/solr/data/blacklight-core/conf/solrconfig.old
+ssh solr@metasolr cp /opt/solr-5.5.2/server/solr/blacklight-core/conf/schema.xml /opt/solr-5.5.2/server/solr/blacklight-core/conf/schema.old
+ssh solr@metasolr cp /opt/solr-5.5.2/server/solr/blacklight-core/conf/solrconfig.xml /opt/solr-5.5.2/server/solr/blacklight-core/conf/solrconfig.old
 
 # copy new config
 scp solr/schema.xml solr@sc0.galib.uga.edu:solr-6.4.1/server/solr/configsets/meta/
