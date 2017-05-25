60c19752d00849dc5e6d31c84e29a0d96c1eb70d
Mike Kanning <mikekanning@gmail.com>
2017-05-25 15:36:24 -0400

    update dev solr scripts to use new solr version

--- cycle_solr.sh
+++ cycle_solr.sh
@@ -4,25 +4,25 @@
 # /home/mk/Projects/meta/solr/
 
 # solr config dirs
-# /var/solr/solr-5.5.2/server/solr
+# /var/solr/solr-6.5.1/server/solr
 # /blacklight-core
 # /test-blacklight-core
 
 # backup old solr config
-mv /var/solr/solr-5.5.2/server/solr/blacklight-core/conf/solrconfig.xml /var/solr/solr-5.5.2/server/solr/blacklight-core/conf/old_solrconfig.xml
-mv /var/solr/solr-5.5.2/server/solr/test-blacklight-core/conf/solrconfig.xml /var/solr/solr-5.5.2/server/solr/test-blacklight-core/conf/old_solrconfig.xml
+mv /var/solr/solr-6.5.1/server/solr/blacklight-core/conf/solrconfig.xml /var/solr/solr-6.5.1/server/solr/blacklight-core/conf/old_solrconfig.xml
+mv /var/solr/solr-6.5.1/server/solr/test-blacklight-core/conf/solrconfig.xml /var/solr/solr-6.5.1/server/solr/test-blacklight-core/conf/old_solrconfig.xml
 
 # backup old solr schema
-mv /var/solr/solr-5.5.2/server/solr/blacklight-core/conf/schema.xml /var/solr/solr-5.5.2/server/solr/blacklight-core/conf/old_schema.xml
-mv /var/solr/solr-5.5.2/server/solr/test-blacklight-core/conf/schema.xml /var/solr/solr-5.5.2/server/solr/test-blacklight-core/conf/old_schema.xml
+mv /var/solr/solr-6.5.1/server/solr/blacklight-core/conf/schema.xml /var/solr/solr-6.5.1/server/solr/blacklight-core/conf/old_schema.xml
+mv /var/solr/solr-6.5.1/server/solr/test-blacklight-core/conf/schema.xml /var/solr/solr-6.5.1/server/solr/test-blacklight-core/conf/old_schema.xml
 
 # copy new solr config
-cp /home/mk/Projects/meta/solr/solrconfig.xml /var/solr/solr-5.5.2/server/solr/blacklight-core/conf/solrconfig.xml
-cp /home/mk/Projects/meta/solr/solrconfig.xml /var/solr/solr-5.5.2/server/solr/test-blacklight-core/conf/solrconfig.xml
+cp /home/mk/Projects/meta/solr/solrconfig.xml /var/solr/solr-6.5.1/server/solr/blacklight-core/conf/solrconfig.xml
+cp /home/mk/Projects/meta/solr/solrconfig.xml /var/solr/solr-6.5.1/server/solr/test-blacklight-core/conf/solrconfig.xml
 
 # copy new solr schema
-cp /home/mk/Projects/meta/solr/schema.xml /var/solr/solr-5.5.2/server/solr/blacklight-core/conf/schema.xml
-cp /home/mk/Projects/meta/solr/schema.xml /var/solr/solr-5.5.2/server/solr/test-blacklight-core/conf/schema.xml
+cp /home/mk/Projects/meta/solr/schema.xml /var/solr/solr-6.5.1/server/solr/blacklight-core/conf/schema.xml
+cp /home/mk/Projects/meta/solr/schema.xml /var/solr/solr-6.5.1/server/solr/test-blacklight-core/conf/schema.xml
 
 # restart solr
-/var/solr/solr-5.5.2/bin/./solr restart
+/var/solr/solr-6.5.1/bin/./solr restart
--- dev_start.sh
+++ dev_start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # start solr
-/var/solr/solr-5.5.2/bin/./solr start
+/var/solr/solr-6.5.1/bin/./solr start
+#/var/solr/solr-5.5.2/bin/./solr start
 # start redis
 redis-server --daemonize yes
