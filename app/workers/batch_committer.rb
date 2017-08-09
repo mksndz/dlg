ccf83fa69dcb18328257501caf270577b4fc2681
Mike Kanning <mikekanning@gmail.com>
2017-08-09 07:35:10 -0400

    clean up batch commit job

--- app/workers/batch_committer.rb
+++ app/workers/batch_committer.rb
@@ -1,34 +1,31 @@
+# job to commit a given batch
 class BatchCommitter
 
   @queue = :batch_commit_queue
   @logger = Logger.new('./log/batch_commit.log')
 
   def self.perform(batch_id)
-
-    b = Batch.find batch_id
-
-    unless b
-      @logger.error "Batch with ID #{batch_id} could not be found."
-      raise JobFailedError
-    end
-
-    if b.batch_items.count < 1
-      @logger.error "Batch with ID #{batch_id} could not be committed because it is empty."
-      raise JobFailedError
-    end
-
-    @logger.info "Committing Batch #{b.id} with #{b.batch_items.count}"
-
+    batch = validated_batch batch_id
+    @logger.info "Committing Batch #{batch.id} with #{batch.batch_items.count}"
     begin
-      b.commit
-    rescue => e
-      @logger.error "Committing resulted in an error: #{e.message}"
+      batch.commit
+    rescue StandardError => e
+      fail_with_message "Committing resulted in an error: #{e.message}"
     end
-
     Sunspot.commit_if_dirty
-
     @logger.info 'Finished committing'
+  end
 
+  def self.fail_with_message(msg)
+    @logger.error msg
+    fail JobFailedError
+  end
+
+  def self.validated_batch(batch_id)
+    batch = Batch.find batch_id
+    fail_with_message "Batch with ID #{batch_id} could not be found." unless batch
+    fail_with_message "Batch with ID #{batch_id} could not be committed because it is empty." if batch.batch_items.count < 1
+    batch
   end
 
 end
