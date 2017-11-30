b6885fa19117ecd44039f82314933d5b74029e0d
Mike Kanning <mikekanning@gmail.com>
2017-11-30 17:40:23 -0500

    additional tests for item_cleaner task

--- spec/tasks/item_cleaner_spec.rb
+++ spec/tasks/item_cleaner_spec.rb
@@ -15,9 +15,18 @@ describe 'cleans items' do
       dcterms_description ['Blah<br><a href="mailto:link@mclink.org"><b>blah</b></a><BR/>BLAH']
     end
   end
-  it 'should clean an item and save' do
+  it 'should clean a dirty item and save only that item' do
+    item = Item.last
+    item_updated = item.updated_at
+    other_item = Fabricate(:item,
+                           collection: item.collection,
+                           portals: item.collection.portals)
+    other_item_updated = other_item.updated_at
     Rake::Task['clean_all_items'].invoke
-    expect(Item.last.dcterms_description.first).to eq "Blah\nblah (link@mclink.org)\nBLAH"
+    item.reload
+    expect(item.dcterms_description.first).to eq "Blah\nblah (link@mclink.org)\nBLAH"
+    expect(item.updated_at).not_to eq item_updated
+    expect(other_item.updated_at).to eq other_item_updated
   end
   it 'should change nothing is dry_run flag is true' do
     Rake::Task['clean_all_items'].invoke true
