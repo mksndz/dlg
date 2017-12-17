872be0d45b507920eb6ca8447e95d1423612a12a
Mike Kanning <mikekanning@gmail.com>
2017-12-17 08:32:30 -0500

    update solr config push script

--- update_solrcoud_config.sh
+++ update_solrcoud_config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # copy new schema
-scp solr/schema.xml solr@sc0.galib.uga.edu:solr-6.4.1/server/solr/configsets/meta/
-scp solr/solrconfig.xml solr@sc0.galib.uga.edu:solr-6.4.1/server/solr/configsets/meta/
+scp solr/schema.xml solr@sc0.galib.uga.edu:solr-6.5.1/server/solr/configsets/meta/
+scp solr/solrconfig.xml solr@sc0.galib.uga.edu:solr-6.5.1/server/solr/configsets/meta/
 
