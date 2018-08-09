abbf82ce01db79e5f719ec38e1d5dae618b77706
Mike Kanning <mikekanning@gmail.com>
2018-08-09 16:46:30 -0400

    add fulltext worker to restart script

--- restart_workers.sh
+++ restart_workers.sh
@@ -9,3 +9,4 @@ PIDFILE=./commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=pr
 PIDFILE=./xml_worker.pid BACKGROUND=yes COUNT=2 QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./reindex_worker.pid BACKGROUND=yes QUEUE=reindex RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./optimize_worker.pid BACKGROUND=yes QUEUE=optimize RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./fulltext_worker.pid BACKGROUND=yes QUEUE=fulltext_ingest_queue RAILS_ENV=production bundle exec rake resque:work
