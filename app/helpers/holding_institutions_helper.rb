e8718993d77bee3310a7275c1eae4cb425cdb4fe
Mike Kanning <mikekanning@gmail.com>
2018-08-21 10:45:09 -0400

    holding inst helper. view updates

---
+++ app/helpers/holding_institutions_helper.rb
@@ -0,0 +1,37 @@
+# useful stuff for working with holding institutions
+module HoldingInstitutionsHelper
+  HOLDING_INSTITUTION_TYPES = [
+    'Archives',
+    'Community College',
+    'Consortia',
+    'Corporate archives',
+    'County agencies',
+    'County archives',
+    'Encyclopedias',
+    'Federal agencies',
+    'Federal archives',
+    'Federal museums',
+    'Historical Societies',
+    'International',
+    'Municipal agencies',
+    'Municipal archives',
+    'Museums',
+    'Newspaper archives',
+    'Presidential libraries',
+    'Private collections',
+    'Public libraries',
+    'Publishers',
+    'Society/foundation archives/libraries',
+    'State agencies',
+    'State archives',
+    'State museums',
+    'State libraries',
+    'State/National parks',
+    'High Schools',
+    'Theological organizations',
+    'University archives',
+    'University departments',
+    'University libraries',
+    'University museums'
+  ].freeze
+end
