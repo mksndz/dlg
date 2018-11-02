d3b40f3ad9cd02814f1604c2d7bb3ba6e782778d
Mike Kanning <mikekanning@gmail.com>
2018-11-02 16:07:54 -0400

    API V2 for Features

--- app/controllers/api/v2/features_controller.rb
+++ app/controllers/api/v2/features_controller.rb
@@ -2,7 +2,27 @@ module Api
   module V2
     # API v2 controller for Features
     class FeaturesController < BaseController
-      def index; end
+      VALID_TYPES = %w[tab carousel].freeze
+      def index
+        limit = params[:count] || 10
+        type = validated_type
+        unless type
+          head :bad_request
+          return
+        end
+        features = if type == 'tab'
+                     Feature.tabs
+                   elsif type == 'carousel'
+                     Feature.carousel
+                   end
+        render json: features.limit(limit)
+      end
+
+      private
+
+      def validated_type
+        params[:type].in?(VALID_TYPES) ? params[:type] : nil
+      end
     end
   end
 end
---
+++ spec/request/api/v2/features_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'API V2 for Features', type: :request do
+  headers = { 'X-User-Token' => Rails.application.secrets.api_token }
+  context 'can list using #index' do
+    before(:all) do
+      Fabricate.times 5, :feature # carousel
+      Fabricate.times 5, :tab_feature
+      Fabricate :external_feature # carousel
+      Fabricate :feature, primary: true # carousel
+      Fabricate :primary_tab_feature
+    end
+    it 'returns an array of tab features, properly ordered' do
+      get '/api/v2/features.json', { type: 'tab' }, headers
+      json = JSON.parse(response.body)
+      expect(json.length).to eq 6
+      expect(json.first['primary']).to be true
+    end
+    it 'returns an array of carousel features, properly ordered' do
+      get '/api/v2/features.json', { type: 'carousel' }, headers
+      json = JSON.parse(response.body)
+      expect(json.length).to eq 7
+      expect(json.first['primary']).to be true
+    end
+    it 'returns error code for unsupported type' do
+      get '/api/v2/features.json', { type: 'collection' }, headers
+      expect(response.status).to eq 400
+    end
+  end
+end
