efb112ddb148623eb50af4790d8993a4cc276e73
Mike Kanning <mikekanning@gmail.com>
2019-01-10 15:34:37 -0500

    logic enhancements, basic spec for page processor

--- app/workers/page_processor.rb
+++ app/workers/page_processor.rb
@@ -9,7 +9,7 @@ class PageProcessor
       @results[:status] = 'failed'
       @results[:message] = "Page Ingest with ID = #{page_ingest_id} could not be found."
       @slack.ping "Page ingest (#{@pi.title}) failed: Page Ingest with ID = #{page_ingest_id} could not be found." if Rails.env.production?
-      @pi.results = @results
+      @pi.results_json = @results
       @pi.save
       exit
     end
@@ -19,13 +19,13 @@ class PageProcessor
         record_id = protopage.delete('id')
         item = Item.find_by record_id: record_id
         unless item
-          @results.added << { "#{record_id}": 'Could not find an Item using this record ID.' }
+          @results[:errors] << { "#{record_id}": 'Could not find an Item using this record ID.' }
         end
         page = Page.create protopage.merge(item: item)
         if page.save
-          @results.added << { "#{item.record_id}": "Page #{page.number} successfully added." }
+          @results[:added] << { "#{item.record_id}": "Page #{page.number} successfully added." }
         else
-          @results.added << { "#{item.record_id}": "Page #{page.number} save failed: #{page.errors}" }
+          @results[:errors] << { "#{item.record_id}": "Page #{page.number} save failed: #{page.errors}" }
         end
       end
     rescue StandardError => e
@@ -33,23 +33,23 @@ class PageProcessor
       @results[:status] = 'failed'
       @results[:message] = e.message
       @slack.ping "Page ingest (#{@pi.title}) failed: #{e.message}" if Rails.env.production?
-      @pi.results = @results
+      @pi.results_json = @results
       @pi.save
       exit
     end
     Sunspot.commit
-    if @results.errors.zero?
+    if @results[:errors].empty?
       @results[:status] = 'success'
       @results[:message] = 'All Pages created successfully'
-    elsif @results.errors.any? && @results.added.any?
+    elsif @results[:errors].any? && @results[:added].any?
       @results[:status] = 'partial failure'
       @results[:message] = 'Some Pages failed to be created'
-    elsif @results.errors.any? && @results.added.zero?
+    elsif @results[:errors].any? && @results[:added].zero?
       @results[:status] = 'failed'
       @results[:message] = 'All Pages failed ot be created'
     end
     @pi.finished_at = Time.zone.today
-    @pi.results = @results
+    @pi.results_json = @results
     @slack.ping "Page ingest complete: `#{@pi.title}`" if Rails.env.production?
     @pi.save
   end
--- spec/workers/page_processor_spec.rb
+++ spec/workers/page_processor_spec.rb
@@ -3,25 +3,12 @@ require 'rails_helper'
 describe PageProcessor, type: :model do
   describe '#perform' do
     let(:page_ingest) { Fabricate :page_ingest_with_json }
-    before(:each) do
-      r = Fabricate :empty_repository, slug: 'r1'
-      c = Fabricate(
-        :empty_collection,
-        slug: 'c1', repository: r, portals: r.portals
-      )
-      Fabricate(:item, slug: 'i1') do
-        collection c
-        portals c.portals
-      end
-      Fabricate(:item, slug: 'i2') do
-        collection c
-        portals c.portals
-      end
-    end
     it 'builds pages and saves results' do
       performed = PageProcessor.perform(page_ingest.id)
       page_ingest.reload
       expect(performed).to be_truthy
+      expect(page_ingest.success?).to be_truthy
+      expect(page_ingest.failed?).to be_falsey
     end
   end
 end
