32088f753a62795e7517f3ce020055d1edd9a994
Mike Kanning <mikekanning@gmail.com>
2019-01-11 15:47:09 -0500

    dry up dpla endpoint specs

--- spec/request/dpla_controller_spec.rb
+++ spec/request/dpla_controller_spec.rb
@@ -11,31 +11,35 @@ RSpec.describe 'DPLA Harvesting Support endpoint', type: :request do
     end
     it 'returns records and request specifications' do
       get '/dpla', {}, headers
-      expect(JSON.parse(response.body)['items'].length).to eq 11
-      expect(JSON.parse(response.body)['numFound']).to eq 11
-      expect(JSON.parse(response.body)['nextCursorMark']).not_to be_empty
+      body = JSON.parse(response.body)
+      expect(body['items'].length).to eq 11
+      expect(body['numFound']).to eq 11
+      expect(body['nextCursorMark']).not_to be_empty
     end
     it 'returns records limited by a row param' do
       get '/dpla', { rows: 2 }, headers
-      expect(JSON.parse(response.body)['items'].length).to eq 2
-      expect(JSON.parse(response.body)['numFound']).to eq 11
-      expect(JSON.parse(response.body)['nextCursorMark']).not_to be_empty
+      body = JSON.parse(response.body)
+      expect(body['items'].length).to eq 2
+      expect(body['numFound']).to eq 11
+      expect(body['nextCursorMark']).not_to be_empty
     end
     it 'returns records paginated using cursorMark' do
       get '/dpla', { rows: 2 }, headers
       cursormark = JSON.parse(response.body)['nextCursorMark']
       get '/dpla', { cursormark: cursormark }, headers
-      expect(JSON.parse(response.body)['items'].length).to eq 9
-      expect(JSON.parse(response.body)['numFound']).to eq 11
-      expect(JSON.parse(response.body)['nextCursorMark']).not_to be_empty
+      body = JSON.parse(response.body)
+      expect(body['items'].length).to eq 9
+      expect(body['numFound']).to eq 11
+      expect(body['nextCursorMark']).not_to be_empty
     end
     it 'returns records paginated using cursorMark and limited by a row param' do
       get '/dpla', { rows: 2 }, headers
       cursormark = JSON.parse(response.body)['nextCursorMark']
       get '/dpla', { cursormark: CGI.escape(cursormark), rows: 3 }, headers
-      expect(JSON.parse(response.body)['items'].length).to eq 3
-      expect(JSON.parse(response.body)['numFound']).to eq 11
-      expect(JSON.parse(response.body)['nextCursorMark']).not_to be_empty
+      body = JSON.parse(response.body)
+      expect(body['items'].length).to eq 3
+      expect(body['numFound']).to eq 11
+      expect(body['nextCursorMark']).not_to be_empty
     end
   end
 end
