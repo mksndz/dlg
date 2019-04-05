8deab65d2a63c11d7b39e4bebc64c90b0f518f7b
Mike Kanning <mikekanning@gmail.com>
2019-04-05 14:43:15 -0400

    include solr request string in error output

--- app/controllers/dpla_controller.rb
+++ app/controllers/dpla_controller.rb
@@ -14,7 +14,8 @@ class DplaController < ApplicationController
     #   cursorMark: cursor_mark
     # }
     solr = Blacklight.default_index.connection.uri
-    uri = URI.parse("#{solr}select?cursorMark=#{cursor_mark}&rows=#{rows}&facet=false&fl=id,+collection_titles_sms,+dcterms_provenance_display,+dcterms_title_display,+dcterms_creator_display,+dcterms_subject_display,+dcterms_description_display,+edm_is_shown_at_display,+edm_is_shown_by_display,+dc_date_display,+dcterms_spatial_display,+dc_format_display,+dc_right_display,+dcterms_type_display,+dcterms_language_display,+dlg_subject_personal_display,+dcterms_bibliographic_citation_display,+dcterms_identifier_display,+dc_relation_display,+dcterms_contributor_display,+dcterms_publisher_display,+dcterms_temporal_display,+dcterms_is_part_of_display,+dcterms_rights_holder_display,+dlg_local_right_display,+dcterms_medium_display,+dcterms_extent_display,+created_at_dts,+updated_at_dts&fq=display_b:1&fq=dpla_b:1&fq=class_name_ss:Item&indent=on&sort=id+asc&wt=json")
+    uri_string = "#{solr}select?cursorMark=#{cursor_mark}&rows=#{rows}&facet=false&fl=id,+collection_titles_sms,+dcterms_provenance_display,+dcterms_title_display,+dcterms_creator_display,+dcterms_subject_display,+dcterms_description_display,+edm_is_shown_at_display,+edm_is_shown_by_display,+dc_date_display,+dcterms_spatial_display,+dc_format_display,+dc_right_display,+dcterms_type_display,+dcterms_language_display,+dlg_subject_personal_display,+dcterms_bibliographic_citation_display,+dcterms_identifier_display,+dc_relation_display,+dcterms_contributor_display,+dcterms_publisher_display,+dcterms_temporal_display,+dcterms_is_part_of_display,+dcterms_rights_holder_display,+dlg_local_right_display,+dcterms_medium_display,+dcterms_extent_display,+created_at_dts,+updated_at_dts&fq=display_b:1&fq=dpla_b:1&fq=class_name_ss:Item&indent=on&sort=id+asc&wt=json"
+    uri = URI.parse(uri_string)
     request = Net::HTTP::Get.new(uri)
     request["Accept"] = "application/json"
     req_options = {
@@ -31,7 +32,7 @@ class DplaController < ApplicationController
         nextCursorMark: r['nextCursorMark']
       }
     else
-      render json: { status: 'failed' }
+      render json: { error: "failed with: #{uri_string}" }
     end
   end
 
