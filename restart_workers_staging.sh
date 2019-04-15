2457e0d0b5686d32b38cd05ed4a6cc0fd628df7e
Mike Kanning <mikekanning@gmail.com>
2019-04-15 14:10:04 -0400

    use staging environment when restarting staging workers

---
+++ restart_workers_staging.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+kill -9 `cat commit_worker.pid`
+kill -9 `cat xml_worker.pid`
+kill -9 `cat xml_worker2.pid`
+kill -9 `cat reindex_worker.pid`
+kill -9 `cat optimize_worker.pid`
+kill -9 `cat fulltext_worker.pid`
+kill -9 `cat resave_worker.pid`
+
+PIDFILE=./commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=staging bundle exec rake resque:work
+PIDFILE=./xml_worker.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=staging bundle exec rake resque:work
+PIDFILE=./xml_worker2.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=staging bundle exec rake resque:work
+PIDFILE=./reindex_worker.pid BACKGROUND=yes QUEUE=reindex RAILS_ENV=staging bundle exec rake resque:work
+PIDFILE=./optimize_worker.pid BACKGROUND=yes QUEUE=optimize RAILS_ENV=staging bundle exec rake resque:work
+PIDFILE=./fulltext_worker.pid BACKGROUND=yes QUEUE=fulltext_ingest_queue RAILS_ENV=staging bundle exec rake resque:work
+PIDFILE=./resave_worker.pid BACKGROUND=yes QUEUE=resave RAILS_ENV=staging bundle exec rake resque:work
