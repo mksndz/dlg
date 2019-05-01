687e27ac2a4cc9173de04e95bee7934e68ab9ed4
Mike Kanning <mikekanning@gmail.com>
2019-05-01 14:00:09 -0400

    fix pidfile issue in worker restart job, update .ruby-version

--- restart_workers_production.sh
+++ restart_workers_production.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-kill -9 `cat commit_worker.pid`
-kill -9 `cat xml_worker.pid`
-kill -9 `cat xml_worker2.pid`
-kill -9 `cat reindex_worker.pid`
-kill -9 `cat optimize_worker.pid`
-kill -9 `cat fulltext_worker.pid`
-kill -9 `cat resave_worker.pid`
+kill -9 `cat ./pidfiles/commit_worker.pid`
+kill -9 `cat ./pidfiles/xml_worker.pid`
+kill -9 `cat ./pidfiles/xml_worker2.pid`
+kill -9 `cat ./pidfiles/reindex_worker.pid`
+kill -9 `cat ./pidfiles/optimize_worker.pid`
+kill -9 `cat ./pidfiles/fulltext_worker.pid`
+kill -9 `cat ./pidfiles/resave_worker.pid`
 
-PIDFILE=./commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=production bundle exec rake resque:work
-PIDFILE=./xml_worker.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
-PIDFILE=./xml_worker2.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
-PIDFILE=./reindex_worker.pid BACKGROUND=yes QUEUE=reindex RAILS_ENV=production bundle exec rake resque:work
-PIDFILE=./optimize_worker.pid BACKGROUND=yes QUEUE=optimize RAILS_ENV=production bundle exec rake resque:work
-PIDFILE=./fulltext_worker.pid BACKGROUND=yes QUEUE=fulltext_ingest_queue RAILS_ENV=production bundle exec rake resque:work
-PIDFILE=./resave_worker.pid BACKGROUND=yes QUEUE=resave RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/xml_worker.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/xml_worker2.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/reindex_worker.pid BACKGROUND=yes QUEUE=reindex RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/optimize_worker.pid BACKGROUND=yes QUEUE=optimize RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/fulltext_worker.pid BACKGROUND=yes QUEUE=fulltext_ingest_queue RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/resave_worker.pid BACKGROUND=yes QUEUE=resave RAILS_ENV=production bundle exec rake resque:work
