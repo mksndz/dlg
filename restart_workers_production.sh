4da075bfe583e2cc1015e45f7b088a1c84427aaf
Mike Kanning <mikekanning@gmail.com>
2019-06-14 09:37:00 -0400

    add page processor to worker management script

--- restart_workers_production.sh
+++ restart_workers_production.sh
@@ -7,6 +7,7 @@ kill -9 `cat ./pidfiles/reindex_worker.pid`
 kill -9 `cat ./pidfiles/optimize_worker.pid`
 kill -9 `cat ./pidfiles/fulltext_worker.pid`
 kill -9 `cat ./pidfiles/resave_worker.pid`
+kill -9 `cat ./pidfiles/page_processor_worker.pid`
 
 PIDFILE=./pidfiles/commit_worker.pid BACKGROUND=yes QUEUE=batch_commit_queue RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./pidfiles/xml_worker.pid BACKGROUND=yes QUEUE=xml RAILS_ENV=production bundle exec rake resque:work
@@ -15,3 +16,4 @@ PIDFILE=./pidfiles/reindex_worker.pid BACKGROUND=yes QUEUE=reindex RAILS_ENV=pro
 PIDFILE=./pidfiles/optimize_worker.pid BACKGROUND=yes QUEUE=optimize RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./pidfiles/fulltext_worker.pid BACKGROUND=yes QUEUE=fulltext_ingest_queue RAILS_ENV=production bundle exec rake resque:work
 PIDFILE=./pidfiles/resave_worker.pid BACKGROUND=yes QUEUE=resave RAILS_ENV=production bundle exec rake resque:work
+PIDFILE=./pidfiles/page_processor_worker.pid BACKGROUND=yes QUEUE=page_ingest_queue RAILS_ENV=staging bundle exec rake resque:work
