7a9ca802cdd2ce20cc9fe28c56d31a62279df1d4
Mike Kanning <mikekanning@gmail.com>
2020-02-04 17:18:39 -0500

    update solr entrypoint files to pull all config files from git repo

--- provision/solr-entrypoint.sh
+++ provision/solr-entrypoint.sh
@@ -3,10 +3,15 @@ mkdir /opt/solr/server/solr/configsets/meta/conf
 mkdir /opt/solr/server/solr/configsets/meta/conf/lang
 curl https://gitlab.galileo.usg.edu/infra/solr-configs/meta/raw/master/meta/schema.xml --output /opt/solr/server/solr/configsets/meta/conf/schema.xml
 curl https://gitlab.galileo.usg.edu/infra/solr-configs/meta/raw/master/meta/solrconfig.xml --output /opt/solr/server/solr/configsets/meta/conf/solrconfig.xml
-cp /opt/solr/server/solr/configsets/basic_configs/conf/lang/stopwords_en.txt /opt/solr/server/solr/configsets/meta/conf/stopwords.txt
-cp /opt/solr/server/solr/configsets/basic_configs/conf/lang/stopwords_en.txt /opt/solr/server/solr/configsets/meta/conf/stopwords_en.txt
-cp /opt/solr/server/solr/configsets/basic_configs/conf/synonyms.txt /opt/solr/server/solr/configsets/meta/conf/synonyms.txt
-cp /opt/solr/server/solr/configsets/basic_configs/conf/protwords.txt /opt/solr/server/solr/configsets/meta/conf/protwords.txt
+curl https://gitlab.galileo.usg.edu/infra/solr-configs/meta/raw/master/meta/conf/stopwords.txt --output /opt/solr/server/solr/configsets/meta/conf/stopwords.xml
+curl https://gitlab.galileo.usg.edu/infra/solr-configs/meta/raw/master/meta/conf/stopwords_en.txt --output /opt/solr/server/solr/configsets/meta/conf/stopwords_en.xml
+curl https://gitlab.galileo.usg.edu/infra/solr-configs/meta/raw/master/meta/conf/synonyms.txt --output /opt/solr/server/solr/configsets/meta/conf/synonyms.xml
+curl https://gitlab.galileo.usg.edu/infra/solr-configs/meta/raw/master/meta/conf/protwords.txt --output /opt/solr/server/solr/configsets/meta/conf/protwords.xml
+
+#cp /opt/solr/server/solr/configsets/basic_configs/conf/lang/stopwords_en.txt /opt/solr/server/solr/configsets/meta/conf/stopwords.txt
+#cp /opt/solr/server/solr/configsets/basic_configs/conf/lang/stopwords_en.txt /opt/solr/server/solr/configsets/meta/conf/stopwords_en.txt
+#cp /opt/solr/server/solr/configsets/basic_configs/conf/synonyms.txt /opt/solr/server/solr/configsets/meta/conf/synonyms.txt
+#cp /opt/solr/server/solr/configsets/basic_configs/conf/protwords.txt /opt/solr/server/solr/configsets/meta/conf/protwords.txt
 /opt/solr/bin/solr start -p 8983
 /opt/solr/bin/solr create_core -c meta-test -d meta -p 8983
 /opt/solr/bin/solr stop
