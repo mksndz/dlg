1821bbfc7749046d1234a4b691c836bd2dc7f5f9
Mike Kanning <mikekanning@gmail.com>
2020-02-05 12:04:09 -0500

    setup should work, believe it is possible

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -7,14 +7,11 @@ cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
 # Workaround for phantomjs openssl issue
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=918727
 export OPENSSL_CONF=/dev/null
+chown -R gitlab-runner:gitlab-runner /code
 HOME=/code gem install bundler
 HOME=/code bundle install --jobs=8
 cp /code/config/database.yml.ci /code/config/database.yml
 cp /code/config/blacklight.yml.ci /code/config/blacklight.yml
 cp /code/config/secrets.yml.ci /code/config/secrets.yml
-echo 'Creating database'
-HOME=/code bundle exec rake db:create RAILS_ENV=test
-echo 'Loading schema'
-HOME=/code bundle exec rake db:test:prepare RAILS_ENV=test
-chown -R gitlab-runner:gitlab-runner /code
+HOME=/code bundle exec rake db:setup RAILS_ENV=test
 sudo -E -u gitlab-runner bundle exec rspec --color --format documentation
